2ba124d9cdef41799c3b20e1e905839f8d08bf53
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-05-26 15:38:54 +0200

    fix header style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,19 +8,21 @@
 </script>
 
 <AbbyProvider {data} abby={__abby__}>
-	<header class="bg-pink-300 py-3">
+	<header class="bg-pink-200 py-3 border-b border-pink-400 shadow-sm">
 		<DevTools abby={__abby__} props={{ defaultShow: true }} />
-		<ul class="flex space-x-10 pl-10">
-			<li>
-				<a href="/">Back to index</a>
-			</li>
-			<li>
-				<a href="/home"> Home </a>
-			</li>
-			<li>
-				<a href="/marketing">Marketing</a>
-			</li>
-		</ul>
+		<nav>
+			<ul class="flex space-x-10 pl-10">
+				<li>
+					<a class="hover:text-gray-100 text-black no-underline" href="/">Back to index</a>
+				</li>
+				<li>
+					<a class="hover:text-gray-100 text-black no-underline" href="/home"> Home </a>
+				</li>
+				<li>
+					<a class="hover:text-gray-100 text-black no-underline" href="/marketing">Marketing</a>
+				</li>
+			</ul>
+		</nav>
 	</header>
 	<slot />
 </AbbyProvider>
