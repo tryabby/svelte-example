4c87f023dee0c7596b6d9e827798972947b6bfee
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-05-26 11:45:09 +0200

    apply prettier

--- src/lib/abby.ts
+++ src/lib/abby.ts
@@ -1,15 +1,23 @@
-import { createAbby } from "@tryabby/svelte";
+import { createAbby } from '@tryabby/svelte';
 
-export const { useAbby, AbbyProvider, useFeatureFlag, withAbby , __abby__, getABResetFunction, withDevTools} = createAbby({
-  projectId: "clg0i3xdc0000mfh7lg0mbvnf",
-  currentEnvironment: "default", //process.env.NODE_ENV,
-  tests: {
-    Home: {
-      variants: ["A", "B", "C"],
-    },
-    Marketing: {
-      variants: ["b", "c", "original"],
-    },
-  },
-  flags: ["clientFlag", "serverFlag"],
+export const {
+	useAbby,
+	AbbyProvider,
+	useFeatureFlag,
+	withAbby,
+	__abby__,
+	getABResetFunction,
+	withDevTools
+} = createAbby({
+	projectId: 'clg0i3xdc0000mfh7lg0mbvnf',
+	currentEnvironment: 'default', //process.env.NODE_ENV,
+	tests: {
+		Home: {
+			variants: ['A', 'B', 'C']
+		},
+		Marketing: {
+			variants: ['b', 'c', 'original']
+		}
+	},
+	flags: ['clientFlag', 'serverFlag']
 });
